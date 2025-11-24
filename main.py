8e379136a337cbddceb478dd8e5cf0cc54e6e945
eBvel <belyakov041098@gmail.com>
2025-11-24 16:51:05 +0700

    Added a delay befor click Login button. Added closing browser after a delay of 5 seconds.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -20,12 +22,16 @@ driver.maximize_window()
 user_name = "standard_user"
 user_password = "secret_sauce"
 # Находим и сохраняем ссылки на поля: "Username" и "Password"
-user_name_field = driver.find_element(By.ID, "user-name")
-user_password_field = driver.find_element(By.ID, "password")
+user_name_field = driver.find_element(By.XPATH, "//input[@name='user-name']")
+user_password_field = driver.find_element(By.XPATH, "//input[@name='password']")
 # Находим и сохраняем ссылку на кнопку "Login"
-login_button = driver.find_element(By.ID, "login-button")
+login_button = driver.find_element(By.XPATH, "//input[@name='login-button']")
 # Заполняем поля: "Username" и "Password" - логинными данными пользователя
 user_name_field.send_keys(user_name)
 user_password_field.send_keys(user_password)
-# Воспроизводим нажатие кнопки "Login"
+# Ждем 2-е секунды и воспроизводим нажатие кнопки "Login"
+time.sleep(2)
 login_button.click()
+# Закрываем браузер через 5 секунд
+time.sleep(5)
+driver.close()
